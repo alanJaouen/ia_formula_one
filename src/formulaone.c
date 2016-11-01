acb15d401c4a7763316536a911424993880f8218
Axel Banal <banal_a@epita.fr>
2016-11-01 01:56:00 +0100

    [rendu] day 1

--- src/formulaone.c
+++ src/formulaone.c
@@ -4,8 +4,19 @@
 
 enum move update(struct car *car)
 {
+    int x = 0;
+    int y = 0;
+    for (int i = 0; i < car->map->width; i++)
+        for (int j = 0; j < car->map->height; j++)
+        {
+            if (map_get_floor(car->map, i, j) == FINISH)
+            {
+              x = i;
+              y = j;
+            }
+        }
     float angle = 
-            get_angle(car->direction, set_angle(car->direction, 44, 3));
+            get_angle(car->direction, set_angle(car->direction, x, y));
 
     if (angle > -0.5 && angle < 0.5)
       return ACCELERATE;
